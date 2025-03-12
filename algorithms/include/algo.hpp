ac9fc75996352c7f885d0735bafc110e59e6b046
Lemonrindstone <Hughman.wright@gmail.com>
2025-03-12 16:33:58 -0500

    algo done
    made changes to Fibonacci and committed from teacher's main

--- algorithms/include/algo.hpp
+++ algorithms/include/algo.hpp
@@ -5,24 +5,35 @@
 #include <iostream>
 #include <string>
 
-long fibonacci(const int n)
-{
-    if(n == 0 || n == 1)
-    {
-        return n;  
+long fibonacci(int n) {
+    if (n == 0 || n == 1) {
+        return n;
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+
+    long a = 0, b = 1;
+    for (int i = 2; i <= n; ++i) {
+        long temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 }
 
-int linear_search(Array * a, const int target)
-{ 
-    std::cout << a->data[0] << std::endl;
-    return target;
+int linear_search(Array* a, const int target) {
+    for (int i = 0; i < a->len; i++) {
+        if (a->data[i] == target) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-long factorial(const int n)
-{
-    return n;
+long factorial(const int n) {
+    if (n == 0)
+        return 1;
+    else {
+        return n * factorial(n - 1);
+    }
 }
 
 std::string fizz_buzz_checker(const int n) {
